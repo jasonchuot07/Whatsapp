7593f054d7678356782ec8a932d3949699a43816
jasonchuot <jasonchuot07@gmail.com>
2021-07-13 03:08:43 +0700

    fourth commit

--- components/Loading/Loading.js
+++ components/Loading/Loading.js
@@ -12,7 +12,7 @@ function Loading() {
     return (
         <div className={styles.loading}>
             <div className={styles.loading_container}>
-                <Image src='https://firebasestorage.googleapis.com/v0/b/whatsapp-c333e.appspot.com/o/logo%2Fwhatsapp.png?alt=media&token=69a4651b-bf6a-489f-b4d8-7c09193d0a4a' alt="whatsapp logo" height={200} width={200} style={{marginBottom: 10}} />
+                <Image src='/public/whatsapp.png' alt="whatsapp logo" height={200} width={200} style={{marginBottom: 10}} />
                 <ClipLoader color='#56c79d' size={50} loading={true} />
             </div>
         </div>
