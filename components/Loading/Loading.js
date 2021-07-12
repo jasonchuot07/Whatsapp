3a1f681de70a4c85bc6ecbbd9ccde2b593720381
jasonchuot <jasonchuot07@gmail.com>
2021-07-13 03:01:03 +0700

    third commit

--- components/Loading/Loading.js
+++ components/Loading/Loading.js
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 import styles from './Loading.module.css'
 import Head from 'next/head'
 import {ClipLoader} from 'react-spinners'
+import Image from 'next/image'
 
 function Loading() {
     useEffect(() => {
@@ -11,7 +12,7 @@ function Loading() {
     return (
         <div className={styles.loading}>
             <div className={styles.loading_container}>
-                <img src='https://firebasestorage.googleapis.com/v0/b/whatsapp-c333e.appspot.com/o/logo%2Fwhatsapp.png?alt=media&token=69a4651b-bf6a-489f-b4d8-7c09193d0a4a' alt="whatsapp logo" height={200} width={200} style={{marginBottom: 10}} />
+                <Image src='https://firebasestorage.googleapis.com/v0/b/whatsapp-c333e.appspot.com/o/logo%2Fwhatsapp.png?alt=media&token=69a4651b-bf6a-489f-b4d8-7c09193d0a4a' alt="whatsapp logo" height={200} width={200} style={{marginBottom: 10}} />
                 <ClipLoader color='#56c79d' size={50} loading={true} />
             </div>
         </div>
